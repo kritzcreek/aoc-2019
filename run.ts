7b417c60b8950d675112f096646999c2b44476f9
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2025-12-06 14:22:43 +0100

    Initial commit

---
+++ run.ts
@@ -0,0 +1,31 @@
+if (Deno.args.length !== 1) {
+  console.error("Please provide a file path as an argument");
+  Deno.exit(1);
+}
+
+let start = performance.mark("start");
+
+const encoder = new TextEncoder();
+const imports = {
+  env: {
+    log: (arg: number) => console.log(arg),
+    log_int: (arg: number) => console.log(arg),
+    log_f32: (arg: number) => console.log(arg),
+    log_float: (arg: number) => console.log(arg),
+    print_char: (cp: number) =>
+      Deno.stdout.writeSync(encoder.encode(String.fromCodePoint(cp))),
+    random: () => Math.random(),
+  },
+};
+
+const filePath = Deno.args[0];
+// TODO catch?
+const wasmCode = await Deno.readFile(filePath);
+
+const wasmModule = new WebAssembly.Module(wasmCode);
+const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
+const main = (wasmInstance.exports["main"] ??
+  wasmInstance.exports["main::main"]) as CallableFunction;
+main();
+let perf = performance.measure("runtime", "start");
+console.log(`${perf.duration.toFixed(2)}ms`);
